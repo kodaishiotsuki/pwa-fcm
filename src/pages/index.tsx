f05122316e408e1c2c53fa7f81ea129918dcef26
kodaishiotsuki <0711dr5@gmail.com>
2023-04-20 18:15:01 +0900

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,16 @@ import { notifyMe } from "@/utils/notify";
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <button onClick={notifyMe}>Click me to get a notification</button>;
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={notifyMe}
+      >
+        Click me to get a notification
+      </button>
+    </div>
+  );
 };
 
 export default Home;
