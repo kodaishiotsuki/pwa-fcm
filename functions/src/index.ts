a1e5d55ac69451f4767a7998103ddc1b0da496a0
kodaishiotsuki <0711dr5@gmail.com>
2023-04-21 18:41:17 +0900

    FCM

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,3 @@
-import * as functions from "firebase-functions";
+import {initializeApp} from "firebase-admin/app";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!");
-  response.send("Hello from Firebase!");
-});
+initializeApp();
