ddf8ded1a825b971f3aca889838da5ef5f54d64e
kodaishiotsuki <0711dr5@gmail.com>
2023-04-21 19:49:24 +0900

    FCM

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -4,7 +4,7 @@ importScripts(
   "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
 );
 
-const firebaseConfig = {
+firebase.initializeApp({
   apiKey: "AIzaSyC38YI6xgX90wpZPyJLefE3kEYnZRlGLBc",
   authDomain: "fcm-app-5235c.firebaseapp.com",
   projectId: "fcm-app-5235c",
@@ -12,11 +12,9 @@ const firebaseConfig = {
   messagingSenderId: "382618525647",
   appId: "1:382618525647:web:621e77c4dcbea809be7cba",
   measurementId: "G-THZYZS6RP4",
-};
+});
 
-const app = firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging(app);
-
+const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
   console.log(
